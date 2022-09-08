98260ce67c43b19293c47a914e35b07e13a61980
matsudaruka <111216744+matsudaruka@users.noreply.github.com>
2022-09-08 20:24:14 +0800

    Add files via upload

---
+++ app/components/sidebar/sidebar.component.ts
@@ -0,0 +1,45 @@
+import {Component, OnInit} from '@angular/core';
+
+declare const $: any;
+
+declare interface RouteInfo {
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+}
+
+export const ROUTES: RouteInfo[] = [
+  {path: '/dashboard', title: 'Dashboard', icon: 'dashboard', class: ''},
+  {path: '/organisations', title: 'Organisations', icon: 'content_paste', class: ''},
+  {path: '/patients', title: 'Patients', icon: 'person', class: ''},
+  {path: '/studies', title: 'Studies', icon: 'library_books', class: ''},
+  {path: '/user-profile', title: 'User Profile', icon: 'person', class: ''},
+  {path: '/table-list', title: 'Table List', icon: 'content_paste', class: ''},
+  {path: '/typography', title: 'Typography', icon: 'library_books', class: ''},
+  {path: '/icons', title: 'Icons', icon: 'bubble_chart', class: ''},
+  {path: '/notifications', title: 'Notifications', icon: 'notifications', class: ''},
+];
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+  menuItems: any[];
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
+  }
+
+  isMobileMenu() {
+    if ($(window).width() > 991) {
+      return false;
+    }
+    return true;
+  }
+}
